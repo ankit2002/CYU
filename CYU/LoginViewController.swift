7b10da621e4f67d06efaae82c0174de430abd4be
Ankit Mishra <ankitmishra@Ankits-MBP.eduroam.uni-kiel.de>
2017-07-13 15:06:03 +0200

    Added Validation for Login Page

--- CYU/LoginViewController.swift
+++ CYU/LoginViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController,UITextFieldDelegate {
 
+    
+    // MARK: Outlet Variables
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
+    // MARK: System Methods
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // hide keyboard when tap arround
+        self.hideKeyboardWhenTappedArround()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,13 +29,85 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //MARK: UITextField Delegates
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if(textField == username){
+            if (textField.text?.isEmpty)!{
+                alert(message: "Please Enter your Email Address")
+                return false;
+            }
+            else if (!self.checkEmailAddress(str: username.text!)){
+                username.becomeFirstResponder()
+                alert(message: "OOPs, Seems like your email Address is not Correct")
+                return false
+            }
+            // Error Check next tag failed
+            let nextTextField:UITextField = self.view.viewWithTag(textField.tag+1) as! UITextField
+            nextTextField.becomeFirstResponder()
+            return true
+        }
+        if(textField == password){
+            if (textField.text?.isEmpty)!{
+                alert(message: "Please Enter your password")
+                return false;
+            }
+            textField.resignFirstResponder()
+            return true
+        }
+        
+        return true
+    }
     
-    // Login buttomn Pressed
+    
+    
+    
+    //MARK: Login button Pressed
     @IBAction func loginBtnPressed(_ sender: Any) {
-        // open Home Menu
+
+        if self.checkValidation(){
+            // open Home Menu
+            self.openHomeView()
+        }
+        else{
+            // TODO: Business logic to check from DB
+            alert(message: "Username and password are incorrect")
+        }
+    }
+    
+    // MARK: Check Validation
+    func checkValidation() -> Bool {
+        if (username.text?.isEmpty)!{
+            username.becomeFirstResponder()
+            alert(message: "Please Enter your EMail")
+            return false
+        }
         
-        // create viewController code...
+        if (!self.checkEmailAddress(str: username.text!)){
+            username.becomeFirstResponder()
+            alert(message: "OOPs, Seems like your email Address is not Correct")
+            return false
+        }
         
+        if (password.text?.isEmpty)!{
+            password.becomeFirstResponder()
+            alert(message: "Please Enter your Password")
+            return false
+        }
+        
+        return true
+    }
+
+    func checkEmailAddress(str:String) -> Bool {
+        let regExForEmail = "[A-Z0-9a-z._%+-]+@[A-Z0-9a-z.-]+\\.[A-Za-z]{2,}"
+        let checkPredicate = NSPredicate (format: "SELF MATCHES %@", regExForEmail)
+        return checkPredicate.evaluate(with:str)
+    }
+    
+    
+    
+    func openHomeView(){
+    // create viewController code...
+    
         let appDelegate : AppDelegate = UIApplication.shared.delegate as! AppDelegate
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -35,13 +115,9 @@ class LoginViewController: UIViewController {
         let mainViewController = storyboard.instantiateViewController(withIdentifier: "StudentHomeViewController") as! StudentHomeViewController
         let leftViewController = storyboard.instantiateViewController(withIdentifier: "SlideMenuLeftViewController") as! SlideMenuLeftViewController
         
-        
         let nvc: UINavigationController = UINavigationController(rootViewController: mainViewController)
         
-        UINavigationBar.appearance().tintColor = UIColor.blue
-        
-        
-        UINavigationBar.appearance().tintColor = UIColor (hex: "689F38")
+        //        UINavigationBar.appearance().tintColor = UIColor (hex: "689F38")
         
         leftViewController.mainViewController = nvc
         
@@ -52,10 +128,8 @@ class LoginViewController: UIViewController {
         appDelegate.window?.backgroundColor = UIColor.white
         appDelegate.window?.rootViewController = slideMenuController
         appDelegate.window?.makeKeyAndVisible()
-        
-        
     }
-
+    
     /*
     // MARK: - Navigation
 
