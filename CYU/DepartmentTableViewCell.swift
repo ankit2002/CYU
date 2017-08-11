2a2915cd2108113b1b86effc6a3e4c5d283d6631
Ankit Mishra <ankitmishra@Ankits-MBP.eduroam.uni-kiel.de>
2017-08-11 16:40:43 +0200

    New DB uploaded ANd Till DepartmentScreen Data Fetched and Shown

---
+++ CYU/DepartmentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DepartmentTableViewCell.swift
+//  CYU
+//
+//  Created by Ankit Mishra on 11/08/17.
+//  Copyright © 2017 Ankit Mishra. All rights reserved.
+//
+
+import UIKit
+
+class DepartmentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
