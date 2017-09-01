e9cff487d43f96bc4d3c75e3e4e82575290b3927
Ankit Mishra <ankitmishra@Ankits-MBP.eduroam.uni-kiel.de>
2017-09-01 18:28:51 +0200

    adding dummy data for filter&search

--- CYU/WelcomeViewController.swift
+++ CYU/WelcomeViewController.swift
@@ -18,7 +18,7 @@ class WelcomeViewController: UIViewController {
         
         //MARK: DO NOT OPEN OR DELETE
         // Creating dummy database 
-//  //      createFirebaseData()
+//        createFirebaseData()
 //      removeFirebaseData()
     }
     
@@ -27,23 +27,49 @@ class WelcomeViewController: UIViewController {
     func createFirebaseData(){
         // And upload data
         // save data of user
+        
+        let countryName = ["Kenya","Syrian Arab Republic","India","United States","United States","Pakistan","India","Sri Lanka","United Kingdom","United States"]
+        let education_level = ["Master","Bachelor","Ph.d"]
+        let courseDesp = ["Master_of_Computer_Science","Bachelor of Computer Science","Sprachen"]
+        let lang = ["English","German","Hindi"]
+        let duration = ["1 Semester","2 Semester","4 Semester"]
+        let fee = ["300 euro","700 euro","100 euro"]
+        let discipline = ["computer","information technology","engineering"]
+        let uniName = ["University of Kabianga","University of Kalamoon","University of Kalyani","University of Kansas","University of Kansas School of Medicine","University of Karachi","University of Kashmir","University of Kelaniya","University of Kent at Canterbury","University of Kentucky"]
+        
+
         var ref: DatabaseReference!
-        ref = Database.database().reference()
+        ref = Database.database().reference().child("filter&search")
         
-        let dict = ["department_head":"A M",
-                    "department_name":"Faculty of Computer Science"
-        ]
         
-        // update Root child name
-        ref.child("testingDB").child("South_Arkansas_Community_College").setValue(dict)
+        for i in 0...9{
+            
+            let randomint = Int(arc4random_uniform(3))
         
+            let dict = ["country":countryName[i],
+                        "education_level":education_level[randomint],
+                        "program_name":courseDesp[randomint],
+                        "program_language":lang[randomint],
+                        "program_duration":duration[randomint],
+                        "tution_fee":fee[randomint],
+                        "discipline":discipline[randomint],
+                        "uni_name": uniName[i]]
+            
+            
+            let uni_Name = uniName[i].replacingOccurrences(of: " ", with: "_", options: .literal, range: nil)
+            let course_Desp=courseDesp[randomint].replacingOccurrences(of: " ", with: "_", options: .literal, range: nil)
+            let keyName = String("\(uni_Name)+\(course_Desp)")!
+            // update Root child name
+            ref.child(keyName).setValue(dict)
+        }
+
     }
     
     //MARK: Remove root node
     func removeFirebaseData(){
         var ref: DatabaseReference!
         ref = Database.database().reference()
-        ref.child("testingDB").setValue(nil);
+        ref.child("filter&search").setValue(nil);
     }
     
     
